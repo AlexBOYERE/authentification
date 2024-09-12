29858f414e1b2719d5bc2d0df45ea2b8ba32a499
AlexBOYERE <proalexandreboyere@gmail.com>
2024-09-12 10:07:53 +0200

    feature(accueil): route accueil

--- app-auth-1/index.js
+++ app-auth-1/index.js
@@ -4,3 +4,22 @@ const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const app = express();
 const port = 3000;
+
+// Utilisateurs enregistrés - faire la transition bdd
+let users = [];
+
+// Middleware pour traiter les requêtes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Définir notre moteur de rendu
+app.set('view engine', 'ejs');
+
+// Route de la page d'accueil
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Bienvenue sur notre site' });
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
