b052533281a854480c1b0f883904713239524951
AlexBOYERE <proalexandreboyere@gmail.com>
2024-09-12 11:38:25 +0200

    feature(register): inscription route

--- app-auth-1/index.js
+++ app-auth-1/index.js
@@ -20,6 +20,28 @@ app.get('/', (req, res) => {
     res.render('index', { message: 'Bienvenue sur notre site' });
 });
 
+// Route d'inscription
+app.get('/register', (req, res) => {
+    res.render('register', { error: null });
+});
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.render('register', { error: 'Utilisateur déjà existant' });
+    }
+
+
+    console.log(password);
+    // Hacher le mot de passe et ajouter l'utilisateur
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    users.push({ username, password: hashedPassword });
+    res.redirect('/login');
+});
+
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
