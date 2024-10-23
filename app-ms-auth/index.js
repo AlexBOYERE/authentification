41c67e36d71ddcacc929d733e2f1fcf2a8aa2c5d
Alexandre BOYERE <57142300+AlexBOYERE@users.noreply.github.com>
2024-10-23 11:12:29 +0200

    chore: remove console.log

--- app-ms-auth/index.js
+++ app-ms-auth/index.js
@@ -57,16 +57,11 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.query;
 
-    console.log('username:', username);
-    console.log('password:', password);
-
     // Vérification si l'utilisateur existe
     const user = await User.findOne({ username });
     if (!user) {
         return res.status(404).json({ message: 'Utilisateur non trouvé' });
     }
-
-    console.log('user:', user);
 
     // Vérification du mot de passe
     const isPasswordValid = bcrypt.compareSync(password, user.password);
@@ -74,14 +69,8 @@ app.post('/login', async (req, res) => {
         return res.status(401).json({ message: 'Mot de passe incorrect' });
     }
 
-    console.log('isPasswordValid:', isPasswordValid);
-    console.log('JWT_SECRET:', JWT_SECRET);
-    console.log('user:', user._id);
-
     // Génération du JWT
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: 86400 }); // Expire en 24h
-
-    console.log('token:', token);
 
     res.json({ message: 'Authentification réussie', token });
 });
